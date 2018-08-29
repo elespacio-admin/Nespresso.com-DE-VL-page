7668b5eae0c475fdf20ec5a1cf55f54d215154b7
jacekzakowicz <jacekzakowicz@gmail.com>
2018-08-29 10:17:16 +0200

    SETUP: removing plugins already loaded via the platform, using JS scripts from Nespresso links, added properly sized header and footer elements emulating platform wrapper.

--- deploy/application/views/templates/footer.php
+++ deploy/application/views/templates/footer.php
@@ -1,6 +1,17 @@
 			</div>
 		<?php if(!$isDistribution): ?>
 			</div>
+			<!--====== SCRIPTS JS ======-->
+			<script src="https://www.nespresso.com/shared_res/mos/free_html/de/vertuo/js/vendor/jquery-1.12.4.min.js"></script>
+      <script src="https://www.nespresso.com/shared_res/mos/free_html/de/vertuo/js/vendor/bootstrap.min.js"></script>
+     <!--====== PLUGINS JS ======-->
+     <script src="https://www.nespresso.com/shared_res/mos/free_html/de/vertuo/js/vendor/jquery.easing.1.3.js"></script>
+     <script src="https://www.nespresso.com/shared_res/mos/free_html/de/vertuo/js/vendor/jquery-migrate-1.2.1.min.js"></script>
+		<?php endif;?>
+		<!--===== ACTIVE JS=====-->
+		<script src="<?php echo $settings['base_url'];?>/assets/scripts/main.js"></script>
+		<?php if(!$isDistribution): ?>
+			<footer class="nespresso-footer"></footer>
 		<?php endif;?>
   </body>
 </html>
--- deploy/application/views/templates/header.php
+++ deploy/application/views/templates/header.php
@@ -3,7 +3,7 @@
   <head>
     <title><?php echo $header['title']; ?></title>
     <meta name="description" content="<?php echo $header['og_description']; ?>">
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 
@@ -18,23 +18,27 @@
     <script src="<?php echo $settings['base_url'];?>/assets/scripts/picturefill.js"></script>
 
     <?php if(!$isDistribution): ?>
-      <!-- Those two CSS files are coming from original/destination page wrapper -->
-      
-      <link rel="stylesheet" type="text/css" href="<?php echo $settings['base_url'];?>/assets/styles/nespresso-original-desktop.css">
-      <link rel="stylesheet" type="text/css" href="<?php echo $settings['base_url'];?>/assets/styles/nespresso-wrapper.css?v=<?php echo $settings['version'];?>">
+      <!--====== MAIN STYLESHEETS ======-->
+      <link rel="stylesheet" href="https://www.nespresso.com/shared_res/mos/free_html/de/vertuo/css/normalize.css">
+      <link rel="stylesheet" href="https://www.nespresso.com/ecom/medias/sys_master/public/11324746399774/animate.css">
+      <link href="https://www.nespresso.com/ecom/medias/sys_master/public/11324747481118/style.css" rel="stylesheet">
+      <link href="https://www.nespresso.com/ecom/medias/sys_master/public/11310902902814/responsive.css" rel="stylesheet">
+      <script src="https://www.nespresso.com/shared_res/mos/free_html/de/vertuo/js/vendor/modernizr-2.8.3.min.js"></script>
+
+      <!-- This CSS files is emulating Nespresso wrapper: geader and footer sizes -->
+      <link rel="stylesheet" type="text/css" href="<?php echo $settings['base_url'];?>/assets/styles/nespresso-wrapper.css">
     <?php endif;?>
 
     <link rel="stylesheet" type="text/css" href="<?php echo $settings['base_url'];?>/assets/styles/main.css?v=<?php echo $settings['version'];?>">
-
-    <script src="<?php echo $settings['base_url'];?>/assets/scripts/main.js"></script>
-    <script src="<?php echo $settings['base_url'];?>/assets/scripts/jquery.1.7.1.min.js"></script>
+    <!-- <script src="<?php //echo $settings['base_url'];?>/assets/scripts/jquery.1.7.1.min.js"></script> -->
   </head>
 
   <body>
     <?php if(!$isDistribution): ?>
     <div class="nespresso-wrapper">
 
-      <div class="black-nespresso-bar"></div>
+      <div class="nespresso-header"></div>
+      <div class="nespresso-header--attached"></div>
     <?php endif;?>
 
       <div id="nvertuo2018" class="">
